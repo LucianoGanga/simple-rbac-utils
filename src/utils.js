03fb00799995ebc9b6a0de198900d8ec904de317
Luciano Ganga <luciano@rodati.com>
2016-08-29 13:55:02 -0300

    Add hasRoles method

--- src/utils.js
+++ src/utils.js
@@ -14,26 +14,28 @@ var User = function(user) {
 		 * @return {Boolean}                Tells if the user can or can't make certain operation over a permission
 		 */
 		can: function(operationName, permissionName) {
-				var can = false;
-				var userPermissions = _.groupBy(user.permissions, 'permission');
-				// Check if user has the permission
-				if (userPermissions[permissionName]) {
-					// Then, check if the user can do that operation on the permission
-					var permission = userPermissions[permissionName];
-					var hasOperation = _.some(permission, function(permissionElement) {
-						return permissionElement.operation === operationName;
-					});
-					if (hasOperation) {
-						can = true;
-					}
+			var can = false;
+			var userPermissions = _.groupBy(user.permissions, 'permission');
+			// Check if user has the permission
+			if (userPermissions[permissionName]) {
+				// Then, check if the user can do that operation on the permission
+				var permission = userPermissions[permissionName];
+				var hasOperation = _.some(permission, function(permissionElement) {
+					return permissionElement.operation === operationName;
+				});
+				if (hasOperation) {
+					can = true;
 				}
-				return can;
 			}
-			// ,
-			// hasRole: function(roleName) {
-			// 	var userRoles = _.groupBy(user.roles, 'name');
-			// 	//if (userRoles.)
-			// }
+			return can;
+		},
+		hasRole: function(roleName) {
+			var userRoles = _.groupBy(user.roles, 'name');
+			if (userRoles.indexOf(roleName) > -1) {
+				return true;
+			}
+			return false
+		}
 	};
 };
 
